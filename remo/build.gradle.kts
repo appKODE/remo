feed5d68ca3154564b856940e33e5de51834cc0a
rinekri <rinekri@gmail.com>
2026-02-07 17:06:23 +0000

    Update deps, migrate to new Gradle and publications

--- remo/build.gradle.kts
+++ remo/build.gradle.kts
@@ -1,18 +1,20 @@
+@file:OptIn(ExperimentalKotlinGradlePluginApi::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
+  id("maven-publish")
   id(libs.plugins.kotlinMultiplatform.get().pluginId)
   id(libs.plugins.dokka.get().pluginId)
-  `maven-publish`
+  id(libs.plugins.vanniktech.maven.publish.get().pluginId)
 }
 
+group = "ru.kode.android"
+version = libs.versions.remoVersion.get()
+
 kotlin {
-  jvm {
-    compilations.all {
-      kotlinOptions {
-        jvmTarget = "1.8"
-        moduleName = "remo-library"
-      }
-    }
-  }
+  jvm()
   iosX64()
   iosArm64()
   iosSimulatorArm64()
@@ -46,10 +48,47 @@ kotlin {
       }
     }
   }
+}
 
-  publishing {
-    publications.withType<MavenPublication> {
+mavenPublishing {
+  coordinates(artifactId = "remo")
+
+  publishToMavenCentral()
+  signAllPublications()
+
+  pom {
+    name.set("remo")
+    description.set("Domain models with reactive repeatable tasks")
+    inceptionYear.set("2022")
+    url.set("https://github.com/appKODE/remo")
+
+    licenses {
+      license {
+        name.set("The Apache License, Version 2.0")
+        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+      }
     }
+
+    developers {
+      developer {
+        id.set("dmitrii.suzdalev.dz")
+        name.set("DIma")
+        email.set("dz@kode.ru")
+      }
+    }
+
+    scm {
+      url.set("https://github.com/appKODE/remo")
+      connection.set("scm:git:https://github.com/appKODE/remo.git")
+      developerConnection.set("scm:git:ssh://git@github.com:appKODE/remo.git")
+    }
+  }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile> {
+  compilerOptions {
+    jvmTarget.set(JvmTarget.JVM_17)
+    moduleName.set("remo-library")
   }
 }
 
@@ -65,3 +104,10 @@ tasks.named<Test>("jvmTest") {
     exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
   }
 }
+
+listOf("iosX64Test", "iosSimulatorArm64Test", "macosX64Test", "macosArm64Test")
+  .forEach { taskName ->
+    tasks.named<AbstractTestTask>(taskName) {
+      enabled = false
+    }
+  }
